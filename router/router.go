fe4fc1fed86ecb44d43e8d822441f2250bbb2b0e
becardine <bealvescardine@gmail.com>
2024-05-23 20:29:24 -0300

    feat: Initialize router and define routes

--- router/router.go
+++ router/router.go
@@ -1,1 +1,22 @@
 package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/becardine/gestock-api/config"
+	"github.com/go-chi/chi/v5"
+)
+
+func Init() {
+	r := chi.NewRouter()
+	initializeRoutes(r)
+
+	webServerPort := config.GetEnv().WebServerPort
+	if webServerPort == "" {
+		webServerPort = "8080"
+	}
+
+	addr := fmt.Sprintf("0.0.0.0:%s", webServerPort)
+	http.ListenAndServe(addr, r)
+}
--- router/routes.go
+++ router/routes.go
@@ -1,1 +1,40 @@
 package router
+
+import (
+	"fmt"
+
+	"github.com/becardine/gestock-api/config"
+	"github.com/becardine/gestock-api/internal/handler"
+	"github.com/becardine/gestock-api/internal/repository"
+	"github.com/becardine/gestock-api/internal/service"
+	"github.com/go-chi/chi/v5"
+)
+
+func initializeRoutes(router *chi.Mux) {
+	// initializa handler
+	err := handler.Initialize()
+	if err != nil {
+		panic(err)
+	}
+
+	basePath := "/api/v1"
+
+	usersPath := fmt.Sprintf("%s/users", basePath)
+
+	db := config.GetDB()
+
+	// repository
+	userRepository := repository.NewUserRepository(db)
+
+	// service
+	userService := service.NewUserService(userRepository)
+
+	// handler
+	handler := handler.NewUserHandler(userService)
+
+	// user routes
+	router.Route(usersPath, func(r chi.Router) {
+		r.Post("/", handler.CreateUser)
+	})
+
+}
