aecad5a94a82869c4879834eefec99ed24d0f698
becardine <bealvescardine@gmail.com>
2024-06-03 22:16:39 -0300

    feat: Add SQL queries and Go code for managing coupons

---
+++ sql/queries/coupon_queries.sql
@@ -0,0 +1,22 @@
+-- name: GetCoupon :one
+SELECT * FROM coupons WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: CreateCoupon :exec
+INSERT INTO coupons (id, code, discount, expiration_date, status, created_date, updated_date)
+VALUES ($1, $2, $3, $4, $5, $6, $7);
+
+-- name: UpdateCoupon :exec
+UPDATE coupons
+SET code = $2, discount = $3, expiration_date = $4, status = $5, updated_date = $6
+WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: DeleteCoupon :exec
+UPDATE coupons
+SET deleted_at = NOW()
+WHERE id = $1;
+
+-- name: ListCoupons :many
+SELECT * FROM coupons WHERE deleted_at IS NULL ORDER BY created_date DESC;
+
+-- name: GetCouponByCode :one
+SELECT * FROM coupons WHERE code = $1 AND deleted_at IS NULL;
