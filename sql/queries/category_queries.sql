e0bbbecfcff07bf477725e259910fb0ad7cd9c93
becardine <bealvescardine@gmail.com>
2024-06-03 22:09:55 -0300

    feat: Add SQL queries for managing categories

---
+++ sql/queries/category_queries.sql
@@ -0,0 +1,26 @@
+-- name: GetCategory :one
+SELECT * FROM categories WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: CreateCategory :exec
+INSERT INTO categories (id, name, description, created_date, updated_date)
+VALUES ($1, $2, $3, $4, $5);
+
+-- name: UpdateCategory :exec
+UPDATE categories
+SET name = $2, description = $3, updated_date = $4
+WHERE id = $1 AND deleted_at IS NULL;
+
+-- name: DeleteCategory :exec
+UPDATE categories
+SET deleted_at = NOW()
+WHERE id = $1;
+
+-- name: ListCategories :many
+SELECT * FROM categories WHERE deleted_at IS NULL ORDER BY name;
+
+-- name: GetCategoryProducts :many
+SELECT p.*
+FROM categories c
+JOIN products p ON c.id = p.category_id
+WHERE c.id = $1 AND c.deleted_at IS NULL
+ORDER BY p.name;
