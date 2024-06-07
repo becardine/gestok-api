e34bd30a75744b89c70404a0ec20460ef100713d
becardine <bealvescardine@gmail.com>
2024-06-07 00:49:44 -0300

    refactor: Update wire.go to use wire.Build for dependency injection

--- wire/wire.go
+++ wire/wire.go
@@ -1,1 +1,24 @@
 package wire
+
+import (
+	"database/sql"
+
+	"github.com/becardine/gestock-api/config"
+	"github.com/becardine/gestock-api/internal/application/handler"
+	"github.com/becardine/gestock-api/internal/domain/service"
+	"github.com/becardine/gestock-api/internal/infra/repository"
+	"github.com/google/wire"
+)
+
+func InitializeProductHandler(db *sql.DB) *handler.ProductHandler {
+	wire.Build(
+		repository.NewProductRepository,
+		service.NewProductService,
+		handler.NewProductHandler,
+	)
+	return &handler.ProductHandler{}
+}
+
+func DBProvider() (*sql.DB, error) {
+	return config.InitializePostgreSQL()
+}
