3343ed7e0a17643a6a1d593d47487ba755337d24
becardine <bealvescardine@gmail.com>
2024-06-07 00:50:04 -0300

    feat: Add createProduct method to ProductHandler

--- internal/application/handler/product.go
+++ internal/application/handler/product.go
@@ -1,1 +1,29 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/becardine/gestock-api/internal/domain/service"
+	"github.com/go-chi/chi/v5"
+)
+
+type ProductHandler struct {
+	productService service.ProductService
+}
+
+func NewProductHandler(productService service.ProductService) *ProductHandler {
+	return &ProductHandler{
+		productService: productService,
+	}
+}
+
+func (h *ProductHandler) createProduct(w http.ResponseWriter, r *http.Request) {
+}
+
+func (h *ProductHandler) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	// Defina as rotas aqui (ex: r.Get("/", h.listProducts))
+
+	return r
+}
