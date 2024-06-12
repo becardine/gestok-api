b454c62284ad604f04e3eabf11309ea1723e0d1b
becardine <bealvescardine@gmail.com>
2024-06-11 23:56:07 -0300

    refactor: Update .gitignore and add brand_test.go and user.go files

---
+++ internal/domain/entity/brand_test.go
@@ -0,0 +1,83 @@
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/becardine/gestock-api/internal/domain/entity"
+	"github.com/becardine/gestock-api/internal/domain/entity/common"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewBrand(t *testing.T) {
+	t.Run("should create a new brand with valid data", func(t *testing.T) {
+		brand, err := entity.NewBrand("Brand Test", "Description Test")
+		assert.NoError(t, err)
+		assert.NotNil(t, brand)
+		assert.Equal(t, "Brand Test", brand.Name)
+		assert.Equal(t, "Description Test", brand.Description)
+	})
+
+	t.Run("should return error if name is empty", func(t *testing.T) {
+		brand, err := entity.NewBrand("", "Description Test")
+		assert.Error(t, err)
+		assert.Nil(t, brand)
+	})
+
+	t.Run("should return error if name exceeds max length", func(t *testing.T) {
+		longName := "This is an extremely long brand name that certainly exceeds the maximum allowed length of 100 characters by a significant margin"
+		brand, err := entity.NewBrand(longName, "Description Test")
+		assert.Error(t, err)
+		assert.Nil(t, brand)
+	})
+
+	t.Run("should return error if description exceeds max length", func(t *testing.T) {
+		longDescription := "This is a very long description that exceeds the maximum allowed length of 255 characters. This is a very long description that exceeds the maximum allowed length of 255 characters. This is a very long description that exceeds the maximum allowed length of 255 characters."
+		brand, err := entity.NewBrand("Brand Test", longDescription)
+		assert.Error(t, err)
+		assert.Nil(t, brand)
+	})
+}
+
+func TestBrand_Validate(t *testing.T) {
+	t.Run("should return no error if brand is valid", func(t *testing.T) {
+		brand := &entity.Brand{
+			ID:          common.NewID(),
+			Name:        "Brand Test",
+			Description: "Description Test",
+		}
+		err := brand.Validate()
+		assert.NoError(t, err)
+	})
+
+	t.Run("should return error if name is empty", func(t *testing.T) {
+		brand := &entity.Brand{
+			ID:          common.NewID(),
+			Name:        "",
+			Description: "Description Test",
+		}
+		err := brand.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("should return error if name exceeds max length", func(t *testing.T) {
+		longName := "This is a very long brand name that exceeds the maximum allowed length of 100 characters"
+		brand := &entity.Brand{
+			ID:          common.NewID(),
+			Name:        longName,
+			Description: "Description Test",
+		}
+		err := brand.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("should return error if description exceeds max length", func(t *testing.T) {
+		longDescription := "This is a very long description that exceeds the maximum allowed length of 255 characters. This is a very long description that exceeds the maximum allowed length of 255 characters. This is a very long description that exceeds the maximum allowed length of 255 characters."
+		brand := &entity.Brand{
+			ID:          common.NewID(),
+			Name:        "Brand Test",
+			Description: longDescription,
+		}
+		err := brand.Validate()
+		assert.Error(t, err)
+	})
+}
--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -3,19 +3,17 @@ package entity
 import (
 	"github.com/becardine/gestock-api/internal/domain/entity/common"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
 
 type User struct {
-	ID       common.ID `json:"id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Name     string    `json:"name" gorm:"not null"`
-	Email    string    `json:"email" gorm:"uniqueIndex;not null"`
-	Password string    `json:"-" gorm:"not null"`
-	gorm.Model
+	ID       common.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"` // - means that this field will not be serialized
 }
 
 func NewUser(name, email, password string) (*User, error) {
-	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +22,7 @@ func NewUser(name, email, password string) (*User, error) {
 		ID:       common.NewID(),
 		Name:     name,
 		Email:    email,
-		Password: string(hasedPassword),
+		Password: string(hashedPassword),
 	}, nil
 }
 
