9a42a1486597935de899cec2d5e085abad4346cb
becardine <bealvescardine@gmail.com>
2024-06-13 20:51:41 -0300

    refactor: Update SQL queries for pagination in product listing

--- internal/infra/queries/product_queries.sql
+++ internal/infra/queries/product_queries.sql
@@ -1,33 +1,41 @@
 -- name: GetProduct :one
-SELECT * FROM products WHERE id = /* sqlc.arg:uuid */ $1 AND deleted_at IS NULL;
+SELECT * FROM products WHERE id = $1 AND deleted_at IS NULL;
 
 -- name: CreateProduct :exec
 INSERT INTO products (id, name, description, price, quantity_in_stock, image_url, category_id, brand_id, created_date, updated_date)
-VALUES (/* sqlc.arg:uuid */ $1, $2, $3, /* sqlc.arg:decimal */ $4, $5, $6, /* sqlc.arg:uuid */ $7, /* sqlc.arg:uuid */ $8, $9, $10);
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
 
 -- name: UpdateProduct :exec
 UPDATE products
-SET name = $2, description = $3, price = /* sqlc.arg:decimal */ $4, quantity_in_stock = $5, image_url = $6, category_id = /* sqlc.arg:uuid */ $7, brand_id = /* sqlc.arg:uuid */ $8, updated_date = $9
-WHERE id = /* sqlc.arg:uuid */ $1 AND deleted_at IS NULL;
+SET name = $2, description = $3, price = $4, quantity_in_stock = $5, image_url = $6, category_id = $7, brand_id = $8, updated_date = $9
+WHERE id = $1 AND deleted_at IS NULL;
 
 -- name: DeleteProduct :exec
 UPDATE products
 SET deleted_at = NOW()
-WHERE id = /* sqlc.arg:uuid */ $1;
+WHERE id = $1;
 
 -- name: ListProducts :many
-SELECT * FROM products WHERE deleted_at IS NULL ORDER BY name;
+SELECT *
+FROM products
+WHERE deleted_at IS NULL
+ORDER BY name
+LIMIT $1 OFFSET $2;
 
--- name: GetProductProducts :many
-SELECT p.*
-FROM products s
-JOIN product_stocks ps ON s.id = ps.product_id
-JOIN stocks p ON ps.stock_id = p.id
-WHERE s.id = /* sqlc.arg:uuid */ $1 AND s.deleted_at IS NULL;
+-- name: GetProductStocks :many
+SELECT s.*
+FROM products p
+         JOIN product_stocks ps ON p.id = ps.product_id
+         JOIN stocks s ON ps.stock_id = s.id
+WHERE p.id = $1 AND p.deleted_at IS NULL AND s.deleted_at IS NULL
+ORDER BY s.name;
 
 -- name: AddProductStock :exec
 INSERT INTO product_stocks (stock_id, product_id)
-VALUES (/* sqlc.arg:uuid */ $1, /* sqlc.arg:uuid */ $2);
+VALUES ($1, $2)
+    ON CONFLICT (stock_id, product_id) DO NOTHING;
 
--- name: RemoveProductStock :exec
-DELETE FROM product_stocks WHERE stock_id = /* sqlc.arg:uuid */ $1 AND product_id = /* sqlc.arg:uuid */ $2;
+-- name: RemoveProductStock :execresult
+DELETE FROM product_stocks
+WHERE stock_id = $1 AND product_id = $2
+    RETURNING *;
--- internal/infra/queries/product_stocks_queries.sql
+++ internal/infra/queries/product_stocks_queries.sql
@@ -1,5 +1,5 @@
 -- name: CreateProductStock :one
-INSERT INTO product_stock (
+INSERT INTO product_stocks (
   id, product_id, stock_id, quantity, created_at, updated_at
 )
 VALUES ($1, $2, $3, $4, $5, $6)
@@ -7,22 +7,22 @@ RETURNING *;
 
 -- name: GetProductStock :one
 SELECT * 
-FROM product_stock 
+FROM product_stocks
 WHERE product_id = $1 AND stock_id = $2 AND deleted_at IS NULL;
 
 -- name: UpdateProductStock :exec
-UPDATE product_stock
+UPDATE product_stocks
 SET quantity = $3, updated_at = $4
 WHERE product_id = $1 AND stock_id = $2 AND deleted_at IS NULL;
 
 -- name: DeleteProductStock :exec
-UPDATE product_stock
+UPDATE product_stocks
 SET deleted_at = NOW()
 WHERE product_id = $1 AND stock_id = $2;
 
 -- name: ListProductStocks :many
 SELECT * 
-FROM product_stock 
+FROM product_stocks
 WHERE product_id = $1 AND deleted_at IS NULL 
 ORDER BY created_at
 LIMIT $2 OFFSET $3; 
@@ -30,7 +30,7 @@ LIMIT $2 OFFSET $3;
 -- name: ListProductsInStock :many
 SELECT p.*
 FROM products p
-JOIN product_stock ps ON p.id = ps.product_id
+JOIN product_stocks ps ON p.id = ps.product_id
 WHERE ps.stock_id = $1 AND ps.deleted_at IS NULL AND p.deleted_at IS NULL
 ORDER BY p.name
 LIMIT $2 OFFSET $3;
