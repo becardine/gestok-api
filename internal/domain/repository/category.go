630593078c5b7b8b98d17bbfb24e8c2adcd45ff1
becardine <bealvescardine@gmail.com>
2024-06-15 20:32:37 -0300

    refactor: Update category repository methods for CRUD operations on categories

--- internal/domain/repository/category.go
+++ internal/domain/repository/category.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"context"
+	"github.com/becardine/gestock-api/internal/domain/entity"
+	"github.com/becardine/gestock-api/internal/domain/entity/common"
+)
+
+type CategoryRepositoryInterface interface {
+	Get(ctx context.Context, id common.ID) (*entity.Category, error)
+	Create(ctx context.Context, category *entity.Category) error
+	Update(ctx context.Context, category *entity.Category) error
+	Delete(ctx context.Context, id common.ID) error
+	List(ctx context.Context, page, pageSize int) ([]*entity.Category, error)
+	GetCategoryProducts(ctx context.Context, categoryID common.ID) ([]*entity.Product, error)
+	//AddCategoryProduct(ctx context.Context, productID, categoryID common.ID) error
+	//RemoveCategoryProduct(ctx context.Context, productID, categoryID common.ID) error
+}
--- internal/infra/repository/category.go
+++ internal/infra/repository/category.go
@@ -1,1 +1,112 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+	"github.com/becardine/gestock-api/internal/domain/entity"
+	"github.com/becardine/gestock-api/internal/domain/entity/common"
+	domain "github.com/becardine/gestock-api/internal/domain/repository"
+	database "github.com/becardine/gestock-api/internal/infra/sqlc"
+	"time"
+)
+
+type CategoryRepository struct {
+	queries *database.Queries
+}
+
+func NewCategoryRepository(db *sql.DB) domain.CategoryRepositoryInterface {
+	return &CategoryRepository{
+		queries: database.New(db),
+	}
+}
+
+func (c CategoryRepository) Get(ctx context.Context, id common.ID) (*entity.Category, error) {
+	category, err := c.queries.GetCategory(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &entity.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: category.Description.String,
+	}, nil
+}
+
+func (c CategoryRepository) Create(ctx context.Context, category *entity.Category) error {
+	err := c.queries.CreateCategory(ctx, database.CreateCategoryParams{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: sql.NullString{String: category.Description, Valid: true},
+		CreatedDate: sql.NullTime{Time: time.Now(), Valid: true},
+		UpdatedDate: sql.NullTime{Time: time.Now(), Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c CategoryRepository) Update(ctx context.Context, category *entity.Category) error {
+	err := c.queries.UpdateCategory(ctx, database.UpdateCategoryParams{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: sql.NullString{String: category.Description, Valid: true},
+		UpdatedDate: sql.NullTime{Time: time.Now(), Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c CategoryRepository) Delete(ctx context.Context, id common.ID) error {
+	err := c.queries.DeleteCategory(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c CategoryRepository) List(ctx context.Context, page, pageSize int) ([]*entity.Category, error) {
+	categories, err := c.queries.ListCategories(ctx, database.ListCategoriesParams{
+		Limit:  int32(pageSize),
+		Offset: int32((page - 1) * pageSize),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*entity.Category
+	for _, category := range categories {
+		result = append(result, &entity.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: category.Description.String,
+		})
+	}
+
+	return result, nil
+}
+
+func (c CategoryRepository) GetCategoryProducts(ctx context.Context, categoryID common.ID) ([]*entity.Product, error) {
+	products, err := c.queries.GetCategoryProducts(ctx, categoryID)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*entity.Product
+	for _, product := range products {
+		result = append(result, &entity.Product{
+			ID:              product.ID,
+			Name:            product.Name,
+			Description:     product.Description.String,
+			QuantityInStock: int(product.QuantityInStock),
+		})
+	}
+
+	return result, nil
+}
