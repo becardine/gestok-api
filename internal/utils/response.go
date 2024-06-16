64f9f046ddd19c37776e7a12589b414dc002d685
becardine <bealvescardine@gmail.com>
2024-06-15 22:55:07 -0300

    refactor: Add response utility function for JSON encoding

---
+++ internal/utils/response.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Data interface{} `json:"data"`
+}
+
+func RespondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	response := Response{Data: data}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		return
+	}
+}
